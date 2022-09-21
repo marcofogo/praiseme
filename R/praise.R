9c21f359434f7e7a19c34dd7c6dc74b2baa1de01
marcofog <marco.fogolin@students.mq.edu.au>
2022-09-21 20:49:55 +1000

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,14 @@
+#' Deliver praise
+#'
+#' @description This function is super useful when you're feeling sad and is useful for everyone.
+#' @param name text string; This is the name of the person to praise.
+#' @param punctuation text string; this is our emphasis as a text input.
+#'
+#' @return Text string with praise
+#' @export
+#'
+#' @examples praise(name = "Steph", punctuation = "!")
+
+praise <- function(name, punctuation = "!") {
+  glue::glue("You're the best, {name}{punctuation}")
+}
---
+++ tests/testthat/test-praise.R
@@ -0,0 +1,7 @@
+testthat::test_that("test praise works", {
+  testthat::expect_identical(praise("Marco"),
+                   glue::glue("You're the best, Marco!"))
+  testthat::expect_identical(praise("Thomas", ";"),
+                   glue::glue("You're the best, Thomas;"))
+  testthat::expect_error(praise())
+})
